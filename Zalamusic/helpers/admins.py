d7aab79ad28db90484743bec32673998a3f95a43
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-11-25 15:00:33 +0530

    Update admins.py

--- Zalamusic/helpers/admins.py
+++ Zalamusic/helpers/admins.py
@@ -19,8 +19,8 @@ from typing import List
 
 from pyrogram.types import Chat, User
 
-from AliciaMusic.function.admins import get as gett
-from AliciaMusic.function.admins import set
+from Zalamusic.function.admins import get as gett
+from ZalaMusic.function.admins import set
 
 
 async def get_administrators(chat: Chat) -> List[User]:
